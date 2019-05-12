9fa8447da367acfcaace1fe671271d70132606f4
Sergey <brazhenkosergey@gmail.com>
2019-05-13 00:19:14 +0300

    Add index and store Categories Back and Front

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -4,21 +4,24 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
 use App\Models\Category;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        $categories = Category::all();
-        return response()->json([
-            'data' => $categories
-        ], 200);
+        $categories = Category::latest()->paginate(10);
+        return response()->json($categories,200);
     }
 
-    public function store(Request $request)
+    public function store(CategoryRequest $categoryRequest)
     {
-        $category = Category::create($request->all());
+
+        $category = Category::create([
+			'name'=> $categoryRequest['name'],
+			'status'=> $categoryRequest['status']
+		]);
         return response()->json(['data' => $category
         ], 201);
     }
--- resources/js/components/Categories.vue
+++ resources/js/components/Categories.vue
@@ -3,87 +3,139 @@
         <div class="row mt-5">
             <div class="col-md-12">
                 <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Cписок категорий</h3>
-				<div class="card-tools">
-					<button class ="btn btn-success" data-toggle="modal" data-target="#addNew">Добавить <i class="nav-icon fa fa-book"></i></button>
-				</div>
-              </div>
-			  
-			
-              <div class="card-body">
-                <table class="table table-hover table-striped">
-                  <tbody><tr>
-                    <th>Номер</th>
-                    <th>Название</th>
-					<th >Статус</th>
-                    <th>Редактировать</th>
-                  </tr>
-				  
-                  <tr>
-                    <td>1</td>
-                    <td>Категория 1</td>
-					<td><span class="badge bg-danger">Заблокирована</span></td>
-                    <td>
-						<a href="#"><i class="fas fa-edit cyan"></i></a>
-						<a href="#"><i class="fas fa-trash red"></i></a>
-					</td>
-                    
-                  </tr>
- 
-                </tbody></table>
-              </div>
-          
-				<nav aria-label="...">
-				  <ul class="pagination justify-content-center">
-					<li class="page-item disabled">
-					  <span class="page-link"><i class="fa fa-chevron-left"></i></span>
-					</li>
-					<li class="page-item"><a class="page-link" href="#"><b>1</b></a></li>
-					<li class="page-item active">
+                    <div class="card-header">
+                        <h3 class="card-title">Cписок категорий</h3>
+                        <div class="card-tools">
+                            <button class="btn btn-success" data-toggle="modal" data-target="#addNew">Добавить <i
+                                    class="nav-icon fa fa-book"></i></button>
+                        </div>
+                    </div>
+
+
+                    <div class="card-body">
+                        <table class="table table-hover table-striped">
+                            <tbody>
+                            <tr>
+                                <th>ID</th>
+                                <th>Название</th>
+                                <th>Статус</th>
+                                <th>Редактировать</th>
+                            </tr>
+
+                            <tr v-for="category in categories" :key="category.id">
+                                <td>{{category.id}}</td>
+                                <td>{{category.name}}</td>
+                                <td><span class="badge bg-danger">Заблокирована</span></td>
+                                <td>
+                                    <a href="#"><i class="fas fa-edit cyan"></i></a>
+                                    <a href="#"><i class="fas fa-trash red"></i></a>
+                                </td>
+
+                            </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <nav aria-label="...">
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item disabled">
+                                <span class="page-link"><i class="fa fa-chevron-left"></i></span>
+                            </li>
+                            <li class="page-item"><a class="page-link" href="#"><b>1</b></a></li>
+                            <li class="page-item active">
 					  <span class="page-link">
 						2
 						<span class="sr-only">(current)</span>
 					  </span>
-					</li>
-					<li class="page-item"><a class="page-link" href="#">3</a></li>
-					<li class="page-item">
-					  <a class="page-link" href="#"><i class="fa fa-chevron-right"></i></a>
-					</li>
-				  </ul>
-				</nav>
-            </div>
+                            </li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#"><i class="fa fa-chevron-right"></i></a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
-		
-		<!-- Modal -->
-		<div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNew" aria-hidden="true">
-		  <div class="modal-dialog" role="document">
-			<div class="modal-content">
-			  <div class="modal-header">
-				<h5 class="modal-title" id="addNew">Добавить категорию</h5>
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-				  <span aria-hidden="true">&times;</span>
-				</button>
-			  </div>
-			  <div class="modal-body">
-				Тут будет форма
-			  </div>
-			  <div class="modal-footer">
-				<button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
-				<button type="button" class="btn btn-primary">Сохранить</button>
-			  </div>
-			</div>
-		  </div>
-		</div>
-		
+
+        <!-- Modal -->
+        <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNew" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addNewH5">Добавить категорию</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+					
+					<form @submit.prevent="createCategory">
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Название категории</label>
+                            <input v-model="form.name" type="text" name="name"
+                                   placeholder="Введите имя"
+                                   class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                            <has-error :form="form" field="name"></has-error>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Статус</label>
+							
+							
+                            <select v-model="form.status" name="status"
+                                    class="form-control" :class="{ 'is-invalid': form.errors.has('status') }">
+                                <option value="1" selected>Активна</option>
+                                <option value="0">Заблокирована</option>
+                            </select>
+                            <has-error :form="form" field="status"></has-error>
+                        </div>
+
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+                        <button type="submit" class="btn btn-primary">Сохранить</button>
+                    </div>
+					
+					</form>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        data() {
+            return {
+                categories:{},
+                form: new Form({
+                    name: '',
+                    status: false
+                })
+            }
+        },
+		
+		methods: {
+
+            showCategories () {
+                axios.get('/api/admin/categories')
+                    .then(({ data }) => { this.categories = data.data })
+            },
+
+			createCategory () {
+			  this.form.post('/api/admin/categories')
+				.then(({ data }) => { console.log(data) })
+			}
+			
+		  },
+
+
+        created() {
+            this.showCategories();
         }
     }
 </script>
